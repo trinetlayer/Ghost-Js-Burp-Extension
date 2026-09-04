6187aa6f82b4af420e99f1dd5bfd584288709c49
Yash <yashhunter772@gmail.com>
2026-09-05 01:05:52 +0530

    Bump version to 1.1.0 for the premium UI release

--- src/main/java/ghostjs/Branding.java
+++ src/main/java/ghostjs/Branding.java
@@ -6,7 +6,7 @@ public final class Branding {
 
     public static final String VENDOR = "TrinetLayer";
     public static final String PRODUCT = "GhostJS";
-    public static final String VERSION = "1.0.0";
+    public static final String VERSION = "1.1.0";
     public static final String TAGLINE = "JS Secret & Endpoint Scanner";
 
     /** Name shown in Burp's Extensions list. */
